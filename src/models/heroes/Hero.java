f50295d75e9eca7d5405b77f2ffbe87f1a1c4a03
defqd <DefQD@yandex.ru>
2024-09-23 18:10:07 +0300

    feat: add a health indicator to the Hero and the opportunity to die

--- src/models/heroes/Hero.java
+++ src/models/heroes/Hero.java
@@ -1,14 +1,17 @@
 package models.heroes;
 
+import interfaces.Mortal;
 import models.enemies.Enemy;
 
-public abstract class Hero {
+public abstract class Hero implements Mortal {
     private String name;
     private int strength;
+    private int health;
 
-    public Hero(String name, int strength) {
+    public Hero(String name, int strength, int health) {
         this.name = name;
         this.strength = strength;
+        this.health = health;
     }
 
     public String getName() {
@@ -17,6 +20,11 @@ public abstract class Hero {
 
     public int getStrength() {
         return strength;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return health > 0;
     }
 
     public abstract void attackEnemy(Enemy enemy);
