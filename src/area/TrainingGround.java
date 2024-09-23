61d5334d0706fe8e00fe07b0b8c1339811296e3c
defqd <DefQD@yandex.ru>
2024-09-23 17:36:02 +0300

    feat: add hero entity

---
+++ src/area/TrainingGround.java
@@ -0,0 +1,10 @@
+package area;
+
+import models.heroes.Hero;
+
+public class TrainingGround {
+    public static void main(String[] args) {
+        var hero = new Hero("Steve");
+        hero.attackEnemy();
+    }
+}
---
+++ src/models/heroes/Hero.java
@@ -0,0 +1,17 @@
+package models.heroes;
+
+public class Hero {
+    private String name;
+
+    public Hero(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public  void attackEnemy() {
+        System.out.printf("%s атакует!!!", name);
+    }
+}
